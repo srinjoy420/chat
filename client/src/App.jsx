2a84c231fa889d0b7f6cdda6e9c20c37a503ae2a
srjonjoy <ghoshsrinjoy2@gmail.com>
2025-07-17 01:04:18 +0530

    finidhed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { use, useEffect, useMemo, useState } from 'react'
+import { io } from "socket.io-client"
+import { Button, Container, TextField, Typography } from "@mui/material"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const socket = useMemo(() => io("http://localhost:8000",{
+    withCredentials:true
+  }), [])
+  const [message, setmessage] = useState("")
+  const [room, setRoom] = useState()
+  //this is for group room
+  const[roomName,setRoomName]=useState("")
+  const [soccketId, setSocketid] = useState("")
+  const [sms, setsms] = useState("")
+  console.log(sms);
 
+  const handelSubmit = (e) => {
+    e.preventDefault()
+    // socket.emit("message",message)
+    socket.emit("message", { message, room })
+    setmessage("")
+
+  }
+  //for the rooms 
+  const handelroom=(e)=>{
+     e.preventDefault()
+    socket.emit("join-room",roomName)
+    //to know that we are join
+    setRoomName("")
+
+  }
+  useEffect(() => {
+    socket.on("connect", () => {
+      setSocketid(socket.id)
+      console.log("connected", socket.id);
+      // socket.on("welcome", (s) => {
+      //   console.log(s);
+
+      // })
+      //the io emit not catcth the data
+      socket.on("recived", (data) => {
+        console.log(data);
+        setsms((sms) => [...sms, data])
+
+      })
+
+    })
+    // return () => {
+    //   socket.disconnect()
+    // }
+  }, [])
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    // <for message
+    <Container maxWidth="sm">
+      <Typography variant="h1" component="div" gutterBottom>
+        welcome to soket.io
+
+      </Typography>
+      <Typography variant="h6" component="div" gutterBottom>
+        {soccketId}
+
+      </Typography>
+
+
+      {/* now for room or group chat */}
+      <form onSubmit={handelroom}>
+          <h5>Join room</h5>
+           <TextField value={roomName} onChange={(e) => setRoomName(e.target.value)} id="outlined-basic" label="roomname" variant="outlined">
+
+        </TextField>
+        <Button type='submit' variant="contained" >
+          Join
+        </Button>
+      </form>
+
+      <form onSubmit={handelSubmit}>
+        <TextField value={message} onChange={(e) => setmessage(e.target.value)} id="outlined-basic" label="message" variant="outlined">
+
+        </TextField>
+        <TextField value={room} onChange={(e) => setRoom(e.target.value)} id="outlined-basic" label="room" variant="outlined">
+
+        </TextField>
+        <Button type='submit' variant="contained" >
+          send
+        </Button>
+      </form>
+      <Typography variant="h6" component="div" gutterBottom>
+        {sms}
+
+      </Typography>
+
+
+    </Container>
   )
 }
 
--- server/src/app.js
+++ server/src/app.js
@@ -1,20 +1,94 @@
 import express from "express"
 import dotenv from "dotenv"
-import {Server} from "socket.io"
-import {createServer} from "http"
+import { Server } from "socket.io"
+import { createServer } from "http"
+import jwt from "jsonwebtoken"
+import cookieParser from "cookie-parser"
+
+import cors from "cors"
+
+dotenv.config()
 
 
-const app=express()
-const server =createServer(app)
-const io=new Server(server)
+const app = express()
+const PORT = process.env.PORT || 8000
+const secret = "hihellokamombolo"
+const server = createServer(app)
+app.use(cookieParser())
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST", "PUT"],
+        credentials: true
 
-dotenv.config
-app.get("/",(req,res)=>{
-    res.json({status:"sucess"})
+    }
+})
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT"],
+    credentials: true
+
+}))
+
+
+app.get("/", (req, res) => {
+    res.json({ status: "sucess" })
+})
+app.get("/login", (req, res) => {
+    const token = jwt.sign({ _id: "fffsss" }, secret)
+    res.cookie("token", token, { httpOnly: true, secure: false }).json({ message: "login succcsfully" })
+})
+const user = false
+io.use((socket, next) => {
+    cookieParser()(socket.request, socket.request.res, (err) => {
+        if (err) return next(err)
+        const token = socket.request.cookies.token
+        if (!token) return next(new Error("authentication error"))
+        const decode = jwt.verify(token, secret)
+        if (!decode) return next(new Error("authentication error"))
+        next()
+
+
+    })
+    
+
 })
 //ioconnection 
-const PORT=process.env.PORT||8000
-app.listen(PORT,()=>{
+io.on("connection", (socket) => {
+    console.log("user connected", socket.id);
+
+
+    // socket.emit("welcome",`hello i am learning the socket ${socket.id}`)
+    // socket.broadcast.emit("welcome",`hello i am learning the socket ${socket.id}`)
+    //message
+    // socket.on("message",(data)=>{
+    //     console.log(data);
+    //     // io.emit("recived",data)
+    //     socket.broadcast.emit("recived",data) // i dont get the  message but all do
+
+    // })
+    //for personal message
+    socket.on("message", ({ room, message }) => {
+        console.log(room, message);
+        // io.emit("recived",data)
+        io.to(room).emit("recived", message) // i dont get the  message but all do
+
+    })
+    //for the join room name
+    socket.on("join-room", (roomName) => {
+        console.log("the room name is ", roomName);
+        socket.join(roomName)
+
+    })
+
+    /// disconnect
+    //      socket.on("disconnect", (reason) => {
+    //     console.log("User disconnected", socket.id, "Reason:", reason);
+    //   });
+
+})
+
+server.listen(PORT, () => {
     console.log(`app is running on port:${PORT}`);
-    
+
 })
